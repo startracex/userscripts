9fdf60db759ab04cca7e465a3d8a6aa8a7c8b05c
Shiro Wang <startraceebr@foxmail.com>
2025-08-13 18:09:06 +0800

    feat(npmjs): 添加在 npmjs.com 显示更多安装命令的脚本

---
+++ packages/npmjs/README.md
@@ -0,0 +1,5 @@
+# npmjs 拓展
+
+此脚本拓展 npmjs 包页面侧栏的安装命令.
+
+![](https://raw.githubusercontent.com/startracex/userscripts/main/docs/npmjs.png)
---
+++ packages/npmjs/src/index.ts
@@ -0,0 +1,50 @@
+const mainElement = document.querySelector("#main");
+const converters = [
+  (cmd: string) => cmd.replace("npm", "pnpm"),
+  (cmd: string) => cmd.replace("npm i", "yarn add"),
+  (cmd: string) => cmd.replace("npm i", "bun add"),
+  (cmd: string) => cmd.replace("npm i", "deno add"),
+  (cmd: string) => cmd.replace("npm i", "npx jsr add"),
+];
+
+const expandInstallElements = () => {
+  const installElement = mainElement.querySelector(":scope > div > :nth-child(4) > h3 + div");
+
+  if (!installElement) {
+    return;
+  }
+
+  const moreElements = converters.map((convert) => {
+    const cloneElement = installElement.cloneNode(true) as HTMLElement;
+    const element = cloneElement.cloneNode(true) as HTMLElement;
+    const code = element.querySelector("code");
+    code.textContent = convert(code.textContent);
+    const button = element.querySelector("button");
+    button.addEventListener("click", async () => {
+      await navigator.clipboard.writeText(code.textContent);
+      const banner = document.createElement("div");
+      banner.className = "fixed top-0 pa3 ph5-ns tl z-999 w-100 flex flex-row justify-between";
+      banner.textContent = "copied to clipboard";
+      banner.style.borderBottom = "1px solid";
+      banner.style.backgroundColor = "var(--color-bg-success)";
+      banner.style.borderColor = "var(--color-border-success)";
+      banner.style.color = "var(--color-fg-success)";
+      document.body.appendChild(banner);
+      setTimeout(() => {
+        banner.remove();
+      }, 2000);
+    });
+    return element;
+  });
+
+  installElement.after(...moreElements);
+};
+
+if (mainElement) {
+  expandInstallElements();
+  const observer = new MutationObserver(expandInstallElements);
+
+  observer.observe(mainElement, {
+    childList: true,
+  });
+}
