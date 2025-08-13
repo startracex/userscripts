f464c8eb79bceedf0da6160c0ffaf37551dd359e
Shiro Wang <startraceebr@foxmail.com>
2025-08-13 18:10:32 +0800

    feat(juejin): 添加屏蔽 juejin.cn 中标题党和广告的脚本

---
+++ packages/juejin/README.md
@@ -0,0 +1,5 @@
+# 掘金扩展
+
+此脚本屏隐藏金主页和推荐等页面中的标题党文章, 以及横幅和侧栏的广告.
+
+![](https://raw.githubusercontent.com/startracex/userscripts/main/docs/juejin.png)
---
+++ packages/juejin/src/index.ts
@@ -0,0 +1,86 @@
+import { createLogFactory } from "@userscripts/shared/log";
+import { waitForQuery } from "@userscripts/shared/query";
+import emojiRegex from "emoji-regex";
+import { block } from "./block.ts";
+
+const info = createLogFactory({
+  level: "INFO",
+  backgroundColor: "#2196F3",
+  textColor: "#ffffff",
+});
+
+const emojiRe = emojiRegex();
+
+const executeHide = (item: HTMLLIElement) => {
+  const title = item.querySelector(".jj-link").getAttribute("title");
+  if (!title) {
+    return;
+  }
+  for (const b of block) {
+    if (b.test(title.replace(emojiRe, ""))) {
+      item.style.display = "none";
+      info`${title} has been hidden because it matches ${b}`;
+      break;
+    }
+  }
+};
+
+[
+  {
+    matches: /^\/(backend|frontend|android|ios|ai|freebie|career|article)(\/.*)?$|^\/$/,
+    handler: () => {
+      waitForQuery(
+        () => document.querySelector(".entry-list"),
+        (list) => {
+          [...list.children].forEach(executeHide);
+          const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+              mutation.addedNodes.forEach((node) => {
+                if (node.nodeType === 1) {
+                  executeHide(node as HTMLLIElement);
+                }
+              });
+            });
+          });
+          observer.observe(list, { childList: true });
+        },
+        250
+      );
+    },
+  },
+  {
+    matches: /^\/post\/.*$/,
+    handler: () => {
+      waitForQuery(
+        () => document.querySelector(`.entry-list.recommended-entry-list`),
+        (list) => {
+          [...list.children].forEach(executeHide);
+        },
+        250
+      );
+      const styleElement = document.createElement("style");
+      styleElement.textContent = ".author-info-block + img { display:none !important }";
+      document.head.appendChild(styleElement);
+    },
+  },
+].forEach((sc) => {
+  const matches = Array.isArray(sc.matches) ? sc.matches : [sc.matches];
+  if (matches.some((match) => match.test(location.pathname))) {
+    sc.handler();
+  }
+});
+
+waitForQuery(
+  () => document.querySelector<HTMLElement>(".global-banner-close"),
+  (e) => {
+    e.click();
+  },
+  250
+);
+
+const styleElement = document.createElement("style");
+styleElement.textContent = `
+.banner-block:has(.close-btn) { display:none }
+.bottom-login-guide { display:none !important }
+`;
+document.head.appendChild(styleElement);
